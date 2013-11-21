c9e0f12d04e2a3003e00c4561315ceacce1643eb
vvasiloud <vvasiloudis@gmail.com>
2013-11-22 01:33:21 +0200

    Integrated Parser to latest UI

--- ClientAlphaPrototype/src/com/example/clientalphaprototype/ScanActivity.java
+++ ClientAlphaPrototype/src/com/example/clientalphaprototype/ScanActivity.java
@@ -1,14 +1,23 @@
 package com.example.clientalphaprototype;
 
+import net.sourceforge.zbar.Symbol;
+
+import com.dm.zbar.android.scanner.ZBarConstants;
+import com.dm.zbar.android.scanner.ZBarScannerActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class ScanActivity extends Activity {
+	private static final int ZBAR_SCANNER_REQUEST = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +34,16 @@ public class ScanActivity extends Activity {
 
 		button.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Intent categories = new Intent(ScanActivity.this, Categories.class);
-				startActivity(categories);
+				//QR Parser
+				 if (isCameraAvailable()) {
+			            Intent intent = new Intent(ScanActivity.this,ZBarScannerActivity.class);
+			            intent.putExtra(ZBarConstants.SCAN_MODES, new int[]{Symbol.QRCODE});
+			            
+			            startActivityForResult(intent, ZBAR_SCANNER_REQUEST);
+			        } else {
+			            Toast.makeText(ScanActivity.this, "Rear Facing Camera Unavailable", Toast.LENGTH_SHORT).show();
+			        }	
+				
 			}
 		});
 	}
@@ -37,5 +54,28 @@ public class ScanActivity extends Activity {
 		getMenuInflater().inflate(R.menu.scan, menu);
 		return true;
 	}
+	
+	public boolean isCameraAvailable() {
+        PackageManager pm = getPackageManager();
+        return pm.hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case ZBAR_SCANNER_REQUEST:
+                if (resultCode == RESULT_OK) {
+                	Intent categories = new Intent(ScanActivity.this, Categories.class);
+    				startActivity(categories);
+                    Toast.makeText(this,"Scan Result = " + data.getStringExtra(ZBarConstants.SCAN_RESULT), Toast.LENGTH_SHORT).show();
+                } else if(resultCode == RESULT_CANCELED && data != null) {
+                    String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
+                    if(!TextUtils.isEmpty(error)) {
+                        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+                    }
+                }
+                break;
+        }
+    }
 
 }
